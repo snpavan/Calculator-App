d1f5d4d8995c0890a0f732d6c76b6cecf8a1908b
Pavan <snpavankumar1998@gmail.com>
2024-11-07 18:32:41 +0530

    Commoting my code

---
+++ calculator.js
@@ -0,0 +1,19 @@
+
+let value = ''; 
+
+function appendDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    try {
+        let display = document.getElementById("display");
+        display.value = eval(display.value);  
+    } catch (error) {
+        display.value = "Error";
+    }
+}
